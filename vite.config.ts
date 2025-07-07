1bd6fe8c4203dee8313bb9fc73154086f2296f2c
Andre Bellmann <andre.bellmann@huk-coburg.de>
2025-07-07 17:19:41 +0200

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/openF1-svelte/',
   plugins: [svelte()],
   test: {
     globals: true,
